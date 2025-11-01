7cf2fe20fc126b83ddef6534e6a6eb06218f017d
Alexandr <AlexandrKsenz@gmail.com>
2025-11-01 21:25:20 +0100

    added header & scripts

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+let body = document.querySelector('body');
+let header = document.querySelector(".header");
+let headerButton = document.querySelector('.header__burger');
+
+headerButton.addEventListener('click', () => {
+  header.classList.toggle('active')
+  body.classList.toggle('locked')
+})
