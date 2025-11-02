de62df6900dff5ba50322059cb5fabf3c2b90862
Alexandr <AlexandrKsenz@gmail.com>
2025-11-02 17:20:06 +0100

    added header bg styles & scripts when scrolled

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,7 +4,14 @@ let body = document.querySelector('body');
 let header = document.querySelector(".header");
 let headerButton = document.querySelector('.header__burger');
 
+window.addEventListener('scroll', () => {
+  window.scrollY > 1
+    ? header.classList.add('active')
+    : header.classList.remove('active')
+})
+
 headerButton.addEventListener('click', () => {
-  header.classList.toggle('active')
+  header.classList.toggle('opened')
   body.classList.toggle('locked')
 })
+
