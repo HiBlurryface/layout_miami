9aae1b8927b359cf3d3ce2f06d29866dd87c10a0
Alexandr <AlexandrKsenz@gmail.com>
2025-11-04 21:15:08 +0100

    added contact section layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,16 @@
-'use strict';
 
 let body = document.querySelector('body');
 let header = document.querySelector(".header");
 let headerButton = document.querySelector('.header__burger');
+
+// var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+// let form = document.querySelector('#contact-form');
+// let nameInput = document.querySelector('#name');
+// let emailInput = document.querySelector('#email');
+// let nameError = document.querySelector('#name-error')
+// let emailError = document.querySelector('#email-error')
+// let messageInput = document.querySelector('#message');
+// let messageError = document.querySelector('#message-error');
 
 window.addEventListener('scroll', () => {
   window.scrollY > 1
@@ -15,3 +23,46 @@ headerButton.addEventListener('click', () => {
   body.classList.toggle('locked')
 })
 
+// form.addEventListener('submit', function (e) {
+//   e.preventDefault();
+
+//   console.log('seccues')
+// })
+
+// function addError(input) {
+//   input.parentElement.classList.add('error');
+// }
+
+// function removeError(input) {
+//   input.parentElement.classList.remove('error');
+// }
+
+// nameInput.addEventListener('input', function () {
+//   if (this.value.length === 0) {
+//     addError(this)
+//     nameError.textContent = 'Field can`t be empty!';
+//   } else {
+//     removeError(this)
+//   }
+// })
+
+// emailInput.addEventListener('input', function () {
+//   if (this.value.length < 1) {
+//     addError(this)
+//     emailError.textContent = 'Field can`t be empty!'
+//   } else if (!regex.test(this.value) && this.value.length > 1) {
+//     addError(this)
+//     emailError.textContent = 'Wrong email addres!'
+//   } else {
+//     removeError(this)
+//   }
+// })
+
+// messageInput.addEventListener('input', function () {
+//   if (this.value.length < 1) {
+//     addError(this);
+//     messageError.textContent = 'Field shoult least minimum 10 symbols!'
+//   } else {
+//     removeError(this);
+//   }
+// })
