a53f9577de7e8d4f3b74cc3a73fd1af75f863667
Alexandr <AlexandrKsenz@gmail.com>
2025-11-05 20:18:51 +0100

    added navigation on page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,12 +2,18 @@
 let body = document.querySelector('body');
 let header = document.querySelector(".header");
 let headerButton = document.querySelector('.header__burger');
+let headerLinks = document.querySelectorAll('.header__menu-link');
+let sections = document.querySelectorAll('[data-section]')
 
-var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 let form = document.querySelector('#contact-form');
 let inputs = document.querySelectorAll('.form__validate');
 let errors = document.querySelectorAll('.form__error-info');
 let toast = document.querySelector('.toast');
+
+let headerHeight = header.offsetHeight;
+var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+// header scripts
 
 window.addEventListener('scroll', () => {
   window.scrollY > 1
@@ -15,10 +21,27 @@ window.addEventListener('scroll', () => {
     : header.classList.remove('active')
 })
 
+headerLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    let section = Array.from(sections).find(el => el.dataset.section === link.dataset.nav);
+    let sectionPosition = section.offsetTop - headerHeight - 40;
+
+    header.classList.remove('opened')
+    body.classList.remove('locked')
+
+    window.scrollTo({
+      top: sectionPosition,
+      behavior: 'smooth'
+    })
+  })
+})
+
 headerButton.addEventListener('click', () => {
   header.classList.toggle('opened')
   body.classList.toggle('locked')
 })
+
+// form scripts
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
@@ -42,13 +65,21 @@ form.addEventListener('submit', function (e) {
       data.push(inputData);
       input.value = "";
     })
-
+    toast.textContent = 'Thank you for contacting us!';
     toast.classList.add('active')
+    toast.classList.remove('error')
     setTimeout(() => {
       toast.classList.remove('active');
     }, 3000);
 
     console.log(data)
+  } else {
+    toast.textContent = 'Incorect form fields!'
+    toast.classList.add('active', 'error')
+    setTimeout(() => {
+      toast.classList.remove('active', 'error');
+    }, 3000);
+    console.log('error')
   }
 })
 
